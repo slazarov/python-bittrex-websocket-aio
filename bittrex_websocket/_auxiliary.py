c74cfbaa67cbc0e75d8e3ad19a3c3b98af2cdc18
slazarov <s.a.lazarov@gmail.com>
2018-04-14 21:27:55 +0300

    Added create_signature method for private callbacks.

--- bittrex_websocket/_auxiliary.py
+++ bittrex_websocket/_auxiliary.py
@@ -6,11 +6,13 @@
 
 import logging
 from uuid import uuid4
+import hashlib
+import hmac
 
 try:
-    from pybase64 import b64decode
+    from pybase64 import b64decode, b64encode
 except:
-    from base64 import b64decode
+    from base64 import b64decode, b64encode
 from zlib import decompress, MAX_WBITS
 
 try:
@@ -27,6 +29,11 @@ async def process_message(message):
     except SyntaxError:
         deflated_msg = decompress(b64decode(message, validate=True))
     return loads(deflated_msg.decode())
+
+
+async def create_signature(api_secret, challenge):
+    api_sign = hmac.new(api_secret.encode(), challenge.encode(), hashlib.sha512).hexdigest()
+    return api_sign
 
 
 class BittrexConnection(object):
